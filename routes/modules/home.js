685e7bbf055544570e950c49144afec8315fb8ee
Derek Hsu <derek800109@gmail.com>
2020-11-10 00:20:41 +0800

    feat: update routes/modules/home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -29,16 +29,17 @@ function get_sort_kv(sort_method) {
 
 router.get('/', (req, res) => {
     const sort_method = req.query.sort || 'asc'
-    console.log("sort_method", sort_method)
+    // console.log("sort_method", sort_method)
 
     const sort_kv = get_sort_kv(sort_method)
+    const userId = req.user._id
 
     // past the movie data into 'index' partial template
-    Restaurant.find() // 取出 Todo model 裡的所有資料
+    Restaurant.find({ userId }) // 取出 Todo model 裡的所有資料
         .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
         .sort(sort_kv) // desc
-        .then((restaurants) => {
-            res.render('index', { restaurants: restaurants })
+        .then(restaurants => {
+            res.render('index', { restaurants })
         }) // 將資料傳給 index 樣板
         .catch(error => console.error(error)) // 錯誤處理
 })
